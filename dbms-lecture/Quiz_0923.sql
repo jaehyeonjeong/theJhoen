9239445a7ac2fd6ceed320e89432856a5526d810
DESKTOP-O1K409M\user <definejae234@gmail.com>
2025-09-23 13:20:32 +0900

    quiz 업데이트

--- dbms-lecture/Quiz_0923.sql
+++ dbms-lecture/Quiz_0923.sql
@@ -32,13 +32,18 @@ SELECT * FROM emp WHERE sal < 2000 OR sal > 3000 ORDER BY sal desc;
 SELECT * FROM emp WHERE sal NOT BETWEEN 2000 AND 3000; -- 만약 쓴다면
 -- Between은 >= AND <= 가 붙으니 유의하여야 함.
 
--- 5
+-- 5 : 사원 이름에 E가 포함된 30번 부서의 사원 중 급여가 1000 ~ 2000 사이가 아닌 데이터 모음
+-- C(insert)R(Select, 얘가 제일 중요해)U(Update)D(delete)
 SELECT empno, ename, sal, deptno FROM emp WHERE deptno = 30 AND
 ename LIKE '%E%' AND sal NOT BETWEEN 1000 AND 2000;
 
---6
+--6 추가 수당이 없고
 SELECT * FROM emp WHERE JOB = 'CLERK' OR JOB = 'MANAGER'
 INTERSECT 
-SELECT * FROM emp WHERE ename NOT LIKE '_L%';
+SELECT * FROM emp WHERE comm IS NULL AND ename NOT LIKE '_L%';
 
-SELECT * FROM emp WHERE JOB = 'CLERK' OR JOB = 'MANAGER' AND ename NOT LIKE '_L%'
+SELECT * FROM emp 
+WHERE comm IS NULL 
+AND JOB = 'CLERK' 
+OR JOB = 'MANAGER' 
+AND ename NOT LIKE '_L%'
