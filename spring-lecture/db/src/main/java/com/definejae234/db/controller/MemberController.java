813a8f92e6d00900151c59b702caef328e7eae52
jaehyeonjeong <definejae234@gmail.com>
2025-10-15 15:31:57 +0900

    Spring을 활용한 DB 연동 후 HTML에 데이터 출력
    추가 : html에서 데이터 입력, 수정, 삭제 (INSERT, UPDATE, DELETE) 진행 및 실습

--- spring-lecture/db/src/main/java/com/definejae234/db/controller/MemberController.java
+++ spring-lecture/db/src/main/java/com/definejae234/db/controller/MemberController.java
@@ -5,8 +5,7 @@ import com.definejae234.db.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,55 @@ public class MemberController {
         Member findedMember = memberRepository.findById(id);
         model.addAttribute("findMember",findedMember);
         return "member/detail";
+    }
+
+    // Get/Post 둘다 사용할 수 있다.
+    @GetMapping("/member/add")
+    public String memberAdd(Model model){
+        // add.html 출력
+        return "member/add";
+    }
+
+    @PostMapping("/member/add")
+//    @ResponseBody
+    public String memberAdd(@ModelAttribute Member member){
+//        System.out.println(member.getUserID()+"==="+
+//                member.getUserName()+"==="+
+//                member.getUserEmail());
+        int result = memberRepository.save(member);
+        System.out.println(result);
+        return "redirect:../member/list";
+    }
+
+    @GetMapping("/member/{id}/edit")
+    public String memberEdit(@PathVariable("id") int id, Model model){
+        // edit.html 출력
+        Member findedMember = memberRepository.findById(id);
+        model.addAttribute("findMember",findedMember);
+        return "member/edit";
+    }
+
+    @PostMapping("/member/edit")
+    public String memberEdit(@ModelAttribute Member member){
+        int result = memberRepository.update(member);
+        System.out.println("MemberController.memberEdit = " + result);
+        return "redirect:../member/list";
+    }
+
+
+    @GetMapping("/member/{id}/delete")
+    public String memberDelete(@PathVariable("id") int id, Model model){
+        // edit.html 출력
+        Member findedMember = memberRepository.findById(id);
+        model.addAttribute("findMember",findedMember);
+        return "member/delete";
+    }
+
+
+    @PostMapping("/member/delete")
+    public String memberDelete(@ModelAttribute Member member){
+        int result = memberRepository.delete(member);
+        System.out.println("MemberController.memberDelete = " + result);
+        return "redirect:../member/list";
     }
 }
--- spring-lecture/db/src/main/java/com/definejae234/db/repository/MemberRepository.java
+++ spring-lecture/db/src/main/java/com/definejae234/db/repository/MemberRepository.java
@@ -25,7 +25,7 @@ public class MemberRepository {
 //    }
 
     public List<Member> findAll() {
-        String sql = "SELECT * FROM MEMBER";
+        String sql = "SELECT * FROM MEMBER ORDER BY ID";
         //SQL 쿼리 결과(ResultSet)를 지정된 Java 클래스의 인스턴스로 변환
         // query : 복수 건 조외
         List<Member> memberList = jdbctemplate.query(sql, new BeanPropertyRowMapper<>(Member.class));
@@ -38,5 +38,25 @@ public class MemberRepository {
         String sql = "SELECT * FROM MEMBER WHERE ID = ?";
         // queryForObject : 단수 건 조외
         return jdbctemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Member.class), id);
+    }
+
+    // 데이터 입력 쿼리
+    // insert, update, delete : DML(Select 제외)일 경우 int를 return
+    public int save(Member member) {
+        // PrepareStatement
+        String sql = "insert into member (id, userID, userName, userEmail) values(member_seq.nextval,?,?,?)";
+        return jdbctemplate.update(sql, member.getUserID(), member.getUserName(), member.getUserEmail());
+    }
+
+    public int update(Member member) {
+        String sql = "update member set userName=?, userEmail=? where id=?";
+        System.out.println("update data = " + member.getId() + ", " + member.getUserName() + ", " + member.getUserEmail() );
+        return jdbctemplate.update(sql, member.getUserName(), member.getUserEmail(), member.getId());
+    }
+
+    public int delete(Member member) {
+        String sql = "delete from member where id=?";
+        System.out.println("MemberRepository.deleteID = " + member.getId());
+        return jdbctemplate.update(sql, member.getId());
     }
 }
