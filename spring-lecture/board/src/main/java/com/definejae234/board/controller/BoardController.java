e1a2e2d36b746b30cd4bf59947a566155f910449
jaehyeonjeong <definejae234@gmail.com>
2025-10-16 16:44:28 +0900

    mybatis 활용 및 연동 spring server 구현
    추가 :
     1. 입력 기능 추가 후 validation과 daisyUI, tailwind를 활용한  입력 웹페이지 스타일 변경

--- spring-lecture/board/src/main/java/com/definejae234/board/controller/BoardController.java
+++ spring-lecture/board/src/main/java/com/definejae234/board/controller/BoardController.java
@@ -2,13 +2,12 @@ package com.definejae234.board.controller;
 
 import com.definejae234.board.dao.BoardDao;
 import com.definejae234.board.dto.BoardDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,7 @@ public class BoardController {
         return "board/list";
     }
 
+    // data write 후 list에 출력
     // Get/Post 둘다 사용할 수 있다.
     @GetMapping("/write")
     public String boardWrite(Model model){
@@ -35,14 +35,23 @@ public class BoardController {
 
     @PostMapping("/write")
 //    @ResponseBody
-    public String boardWriteProcess(@ModelAttribute BoardDto boardDto){
-//        System.out.println(member.getUserID()+"==="+
-//                member.getUserName()+"==="+
-//                member.getUserEmail());
+    public String boardWriteProcess(@Valid @ModelAttribute BoardDto boardDto,
+                                    BindingResult bindingResult,
+                                    Model model){
+        if (bindingResult.hasErrors()) {
+//            model.addAttribute("msg", true);
+            return "board/write";
+        }
         int result = boardDao.writeBoard(boardDto);
         System.out.println(result);
         return "redirect:../board/list";
     }
 
-
+    // list에 있는 데이터의 detail을 출력
+//    @GetMapping("/board/detail/{id}")
+//    public String memberList(@PathVariable("id") int id, Model model){
+//        BoardDto findBoard = boardDao.findById(id);
+//        model.addAttribute("findBoard",findBoard);
+//        return "board/detail";
+//    }
 }
--- spring-lecture/board/src/main/java/com/definejae234/board/dto/BoardDto.java
+++ spring-lecture/board/src/main/java/com/definejae234/board/dto/BoardDto.java
@@ -1,5 +1,8 @@
 package com.definejae234.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,10 +13,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BoardDto {
     private int id;
+
+    @NotBlank(message = "도서의 제목을 입력해주세요")
     private String title;
+    @NotBlank(message = "도서의 내용을 입력해주세요")
     private String content;
+    @NotBlank(message = "도서의 저자를 입력해주세요")
     private String writer;
     private LocalDateTime regDate;
     private int hit;
+    @NotBlank(message = "도서의 비밀번호를 입력해주세요")
+    @Size(min = 4, max = 16, message = "비밀번호는 4글자 이상 16자 이하로 입력해주세요.")
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\\\"'<>,.?/]).+$",
+            message="비밀번호는 영문 대문자/소문자/특수문자/숫자를 모두 포함해야 합니다.")
     private String password;
 }
